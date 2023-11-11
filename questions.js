f8bf251689639752f34b6bb3b972b8b3dd0f8268
Ahmed mohamed salah <erewrrewrewr15@gmail.com>
2023-11-11 17:32:51 +0200

    aswd

---
+++ questions.js
@@ -0,0 +1,90 @@
+let questions = [
+  {
+  numb: 1,
+  question: "C:\\Users\\laptop\\desktop> md newfolder عند تنفيذ الامر التالي ",
+  answer: "desktop في  newfolder انشاء فولدر",
+  options: [
+    "laptop في  newfolder انشاء فولدر",
+    "desktop في  newfolder انشاء فولدر",
+    "desktop في  newfolder انشاء ملف",
+    "laptop في  newfolder انشاء ملف"
+  ]
+},
+{    numb: 2,
+  question: "C:\\Users\\laptop\\desktop> هو root  في المسار التالي يعتبر ال ",
+  answer: "C:\\>",
+  options: [
+    "C:\\Users\\laptop\\desktop>",
+    "C:\\Users\\laptop\\>",
+    "C:\\Users\\>",
+    "C:\\>"
+  ]
+},
+  {
+  numb: 3,
+  question: " الامر الذي يسمح بتكرار الملفات ",
+  answer: "copy",
+  options: [
+    "Md",
+    "copy",
+    "Cd",
+    "Cd.."
+  ]
+},
+  {
+  numb: 4,
+  question: "الامر الذي يستخدم في تحريك الملفات ",
+  answer: "Move",
+  options: [
+    "Move",
+    "copy",
+    "md",
+    "cd"
+  ]
+},
+  {
+  numb: 5,
+  question: "لطباعة كلمة نستخدم اي امر؟",
+  answer: "echo",
+  options: [
+    "echo",
+    "echo<",
+    "echo > ahmed.txt",
+    "rd"
+  ]
+},
+
+  {
+numb: 6,
+question: " لمعرفة اصدار جهاز الكمبيوتر اي امر نستخدم",
+answer: "ver",
+  options: [
+    "ver",
+    "echo",
+    "attrib +h",
+    "attrib -h"
+  ]
+},
+{
+  numb: 7,
+  question: "لعرض محتوي اي ملف نصي نستخدم اي امر؟",
+  answer: "Type",
+    options: [
+      "md",
+      "Type",
+      "Sort",
+      "echo"
+    ]
+  },
+  {
+    numb: 8,
+    question: "يوجد بداخل فولدر ملف نصي اي امر نستعملة لحذف الفولدر",
+    answer: "rd/s",
+      options: [
+        "rd/s",
+        "del",
+        "rd",
+        "md"
+      ]
+    },
+];
